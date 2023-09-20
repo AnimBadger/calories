319bb48030c607bdd16ec129cfbe65877edaec00
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-20 13:42:28 +0000

    pushed broken calories query

--- calories/api/serializers.py
+++ calories/api/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from .models import UserDetail, CaloriesInput
+import os
+import requests
+
+app_id = os.getenv('APPLICATION_ID')
+app_key = os.getenv('APPLICATION_KEY')
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -42,6 +47,29 @@ class CaloriesSerializer(serializers.ModelSerializer):
         username = validated_data.pop('username', None)
 
         user = UserDetail.objects.get(username=username)
+
+        if 'number_of_calories' not in validated_data or validated_data['number_of_calories'] is None:
+            food_name = validated_data['name']
+
+            nutritionix_api_url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+            headers = {
+                "x-app-id": app_id,
+                "x-app-key": app_key,
+                "Content-Type": "application/json",
+            }
+            params = {
+                'query': food_name
+            }
+
+            response = requests.post(
+                nutritionix_api_url, headers=headers, json=params)
+            if response.status_code == 200:
+                nutrition_data = response.json()
+                validated_data['number_of_calories'] = (
+                    nutrition_data['foods'][0]['nf_calories'])
+            else:
+                raise serializers.ValidationError(
+                    {'Error': 'Unable to fetch calories from Nutritionix API'})
 
         validated_data['user'] = user
         instance = self.Meta.model(**validated_data)
