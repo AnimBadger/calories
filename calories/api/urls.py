72ef116b3388b2ecdae94da9886c6dae58390bc5
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-21 13:28:15 +0000

    refactored code

--- calories/api/urls.py
+++ calories/api/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import EndPoints, RegisterUser, LoginUser, Calories
+from .views import EndPoints, RegisterUser, Calories, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
 urlpatterns = [
     path('', EndPoints.as_view()),
-    path('api/login', LoginUser.as_view()),
     path('api/signup', RegisterUser.as_view()),
-    # path('api/token/', CustomTokenObtainPairView.as_view(),
-    #     name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login', MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/calories', Calories.as_view()),
 ]
--- calories/api/views.py
+++ calories/api/views.py
@@ -4,10 +4,24 @@ from .serializers import UserSerializer, CaloriesSerializer
 from rest_framework import status
 from .models import UserDetail, CaloriesInput
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 import os
 import requests
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 app_id = os.getenv('APPLICATION_ID')
@@ -36,26 +50,6 @@ class RegisterUser(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class LoginUser(APIView):
-    def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
-
-        user = UserDetail.objects.filter(email=email).first()
-        if not user or not user.check_password(password):
-            raise AuthenticationFailed('Invalid credentials')
-
-        refresh = RefreshToken.for_user(user)
-        access_token = str(refresh.access_token)
-        response_data = {
-            'refresh': str(refresh),
-            'access': access_token,
-        }
-        response = Response(response_data)
-        response['Authorization'] = f'Bearer {access_token}'
-        return response
 
 
 class Calories(APIView):
